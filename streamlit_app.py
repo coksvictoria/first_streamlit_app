cffc17ddc6bf571adca22352e0c3e70be166f344
coksvictoria <77368342+coksvictoria@users.noreply.github.com>
2023-06-22 14:39:50 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ streamlit.text('🥣 Text 1')
 streamlit.text('🥗 Text 2')
 streamlit.text('🥑 Text 3')
 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(my_fruit_list)
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
