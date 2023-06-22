a46b64469f9ddf8d0cdd43bcb87bd96e8e385d5d
coksvictoria <77368342+coksvictoria@users.noreply.github.com>
2023-06-22 14:38:55 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("Title Name")
 
@@ -6,3 +7,7 @@ streamlit.header('header 1')
 streamlit.text('🥣 Text 1')
 streamlit.text('🥗 Text 2')
 streamlit.text('🥑 Text 3')
+
+
+streamlit.dataframe(my_fruit_list)
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
