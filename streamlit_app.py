896ab228cfba5ed6ad7de514cae9f64c8a9978d4
coksvictoria <77368342+coksvictoria@users.noreply.github.com>
2023-06-30 09:28:51 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas as pd
+import snowflake.connector
+import requests
+from urllib.error import URLError
 
 streamlit.title("Title Name")
 
@@ -17,10 +20,17 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+##display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+#New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input("what fruit you like?")
+streamlit.write('The user entered', fruit_choice)
 
-import snowflake.connector
+fruitvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+streamlit.dataframe(fruitvice_response)
+
 
 ##do not run anything past here while we troubleshoot
 streamlit.stop()
